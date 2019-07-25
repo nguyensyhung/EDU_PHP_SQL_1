67e5d8346e14782c33ef9ba17deff15898ff7e82
Sy Hung <nguyensyhung689@gmail.com>
2019-07-25 21:29:26 +0700

    Use URL parameters

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -9,13 +9,13 @@
   <head>
     <title>GBI - <?php echo $page_title ; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
   </head>
 
   <body>
     <header> <h1>GBI Staff Area</h1> </header>
     <navigation>
       <ul>
-        <li> <a href="index.php">Menu</a></li>
+        <li> <a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
       </ul>
     </navigation>
